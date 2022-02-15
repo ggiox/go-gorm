7cb3fa19365f5182efa4c433e2187b6f263d4965
ggiox <giovanni.aredes@outlook.com>
2022-02-15 15:26:09 -0300

    Added print command

--- sqlite/main.go
+++ sqlite/main.go
@@ -2,6 +2,8 @@
 package main
 
 import (
+	"fmt"
+
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
@@ -13,7 +15,7 @@ type Product struct {
 }
 
 func main() {
-	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	db, err := gorm.Open(sqlite.Open("./db/test.db"), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
 	}
@@ -26,8 +28,11 @@ func main() {
 
 	// Read
 	var product Product
-	db.First(&product, 1)                 // find product with integer primary key
+	//db.First(&product, 1)                 // find product with integer primary key
 	db.First(&product, "code = ?", "D42") // find product with code D42
+
+	// Print product
+	fmt.Println("Id: ", product.ID, "Code: ", product.Code, "Price: ", product.Price)
 
 	// Update - update product's price to 200
 	db.Model(&product).Update("Price", 200)
@@ -35,6 +40,10 @@ func main() {
 	db.Model(&product).Updates(Product{Price: 200, Code: "F42"}) // non-zero fields
 	db.Model(&product).Updates(map[string]interface{}{"Price": 200, "Code": "F42"})
 
+	// Print product
+	fmt.Println("Id: ", product.ID, "Code: ", product.Code, "Price: ", product.Price)
+
 	// Delete - delete product
-	db.Delete(&product, 1)
+	//db.Delete(&product, 1)
+	db.Delete(&product, product.ID)
 }
